1a7a7b138e908bdadde37f1dba5e3690a6f6586f
Vladimir <vladimir@unimate.(none)>
2014-04-24 15:00:42 -0700

    Added yaml files

---
+++ pr2_pbd_interaction/src/pr2_pbd_interaction/Landmark.py
@@ -0,0 +1,3 @@
+class Landmark:
+    def __init__(self):
+        self.object_type = obj_tp
---
+++ pr2_pbd_interaction/src/pr2_pbd_interaction/World.py
@@ -0,0 +1,18 @@
+# get landmarks
+
+class  World(object):
+	"""docstring for  World"""
+	def __init__(self, arg):
+		self.arg = arg
+		self.objectData = []
+
+	def scan_landmarks(self):
+		#get tabletop objects
+		#get tabletop
+		self.objectData = []
+
+	def id_landmarks(self):
+		self.objects = []
+
+	def set_landmark(self, ind, obj):
+		self.objects[ind] = obj
